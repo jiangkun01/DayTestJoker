af76c1ab4c35cfb15920c9238fe47eb5e4a12e98
joker <467288604@qq.com>
2020-03-29 09:30:56 +0800

    lambda 使用场景

--- src/main/java/com/example/test/ww/T20200328.java
+++ src/main/java/com/example/test/ww/T20200328.java
@@ -38,6 +38,12 @@ public class T20200328 {
     // 前 面 已 经 讨 论 过， Java 中 已 经 有 很 多 封 装 代 码 块 的 接 口， 如 ActionListener 或
     //Comparatorlambda 表达式与这些接口是兼容的，
     // 2）构造器引用
+    //ambda 表达式的重点是延迟执行 deferred execution )
+    //•多次运行代码； •在算法的适当位置运行代码（例如， 排序中的比较操作；)
+    // 发生某种情况时执行代码（如， 点击了一个按钮， 数据到达， 等等；) •
+    // 只在必要时才运行代码。
+    //如果你无意中增加了另一个非抽象方法， 编译
+    //器会产生一个错误消息。 另外 javadoc 页里会指出你的接口是一个函数式接口。
     public static void countDown(int start, int delay) {
         ActionListener listener = event -> {
             // Error: Can't mutate captured variable
